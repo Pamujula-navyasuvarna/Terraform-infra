c786515415979b5ef6aca43c3bee985c1fcb7926
Pamujula-navyasuvarna <navyasuvarnap@gmail.com>
2025-11-13 11:22:18 +0530

    Add outputs for VPC and subnets in output.tf

---
+++ Infra-code/output.tf
@@ -0,0 +1,19 @@
+########################## outputs.tf ##########################
+output "vpc_id" {
+value = aws_vpc.this.id
+}
+
+
+output "public_subnet_ids" {
+value = [for s in aws_subnet.public : s.id]
+}
+
+
+output "private_subnet_ids" {
+value = [for s in aws_subnet.private : s.id]
+}
+
+
+output "alb_dns" {
+value = aws_lb.app_alb.dns_name
+}
